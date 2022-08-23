715c2ab7ba8770119bd94b47b8cd827fb933c00e
JADPA-18 <JADPA-18@JADPA-18>
2022-08-23 12:01:40 -0600

    Agregar archivos de proyecto.

---
+++ pjControlFacturas/frmFacturas.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace pjControlFacturas
+{
+    partial class frmFacturas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtVendedor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNuevaFact = new System.Windows.Forms.Button();
+            this.btnRegistrarItem = new System.Windows.Forms.Button();
+            this.txtUnidades = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cboTipo = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNumFact = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lvRegistros = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblMonto = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lbEstadisticas = new System.Windows.Forms.ListView();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader7 = new System.Windows.Forms.ColumnHeader();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(225, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONTROL DE REGISTRO DE FACTURAS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre del vendedor";
+            // 
+            // txtVendedor
+            // 
+            this.txtVendedor.Location = new System.Drawing.Point(84, 75);
+            this.txtVendedor.Name = "txtVendedor";
+            this.txtVendedor.Size = new System.Drawing.Size(286, 23);
+            this.txtVendedor.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(515, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Fecha";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(515, 78);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(38, 15);
+            this.lblFecha.TabIndex = 4;
+            this.lblFecha.Text = "label4";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNuevaFact);
+            this.groupBox1.Controls.Add(this.btnRegistrarItem);
+            this.groupBox1.Controls.Add(this.txtUnidades);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.cboTipo);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtNumFact);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(27, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(750, 135);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "REGISTRO DE FACTURAS";
+            // 
+            // btnNuevaFact
+            // 
+            this.btnNuevaFact.Location = new System.Drawing.Point(589, 78);
+            this.btnNuevaFact.Name = "btnNuevaFact";
+            this.btnNuevaFact.Size = new System.Drawing.Size(111, 23);
+            this.btnNuevaFact.TabIndex = 7;
+            this.btnNuevaFact.Text = "OTRA FACTURA";
+            this.btnNuevaFact.UseVisualStyleBackColor = true;
+            this.btnNuevaFact.Click += new System.EventHandler(this.btnNuevaFact_Click);
+            // 
+            // btnRegistrarItem
+            // 
+            this.btnRegistrarItem.Location = new System.Drawing.Point(589, 25);
+            this.btnRegistrarItem.Name = "btnRegistrarItem";
+            this.btnRegistrarItem.Size = new System.Drawing.Size(111, 23);
+            this.btnRegistrarItem.TabIndex = 6;
+            this.btnRegistrarItem.Text = "REGISTRAR ITEM";
+            this.btnRegistrarItem.UseVisualStyleBackColor = true;
+            this.btnRegistrarItem.Click += new System.EventHandler(this.btnRegistrarItem_Click);
+            // 
+            // txtUnidades
+            // 
+            this.txtUnidades.Location = new System.Drawing.Point(439, 62);
+            this.txtUnidades.Name = "txtUnidades";
+            this.txtUnidades.Size = new System.Drawing.Size(100, 23);
+            this.txtUnidades.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(449, 29);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(63, 15);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "UNIDADES";
+            // 
+            // cboTipo
+            // 
+            this.cboTipo.FormattingEnabled = true;
+            this.cboTipo.Items.AddRange(new object[] {
+            "Cobre",
+            "Bronce",
+            "Silver",
+            "Gold"});
+            this.cboTipo.Location = new System.Drawing.Point(208, 62);
+            this.cboTipo.Name = "cboTipo";
+            this.cboTipo.Size = new System.Drawing.Size(121, 23);
+            this.cboTipo.TabIndex = 3;
+            this.cboTipo.Text = "(Seleccione tipo)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(208, 29);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 15);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "TIPO DE LICENCIA";
+            // 
+            // txtNumFact
+            // 
+            this.txtNumFact.Location = new System.Drawing.Point(8, 62);
+            this.txtNumFact.Name = "txtNumFact";
+            this.txtNumFact.Size = new System.Drawing.Size(111, 23);
+            this.txtNumFact.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 15);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "N° FACTURA";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(39, 294);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(130, 15);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "LISTADO DE REGISTROS";
+            // 
+            // lvRegistros
+            // 
+            this.lvRegistros.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.lvRegistros.GridLines = true;
+            this.lvRegistros.Location = new System.Drawing.Point(39, 324);
+            this.lvRegistros.Name = "lvRegistros";
+            this.lvRegistros.Size = new System.Drawing.Size(582, 178);
+            this.lvRegistros.TabIndex = 7;
+            this.lvRegistros.UseCompatibleStateImageBehavior = false;
+            this.lvRegistros.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Num. Fact";
+            this.columnHeader1.Width = 120;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tipo de licencia";
+            this.columnHeader2.Width = 150;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Unidades";
+            this.columnHeader3.Width = 140;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "SubTotal";
+            this.columnHeader4.Width = 140;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(616, 532);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 15);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "MONTO TOTAL";
+            // 
+            // lblMonto
+            // 
+            this.lblMonto.AutoSize = true;
+            this.lblMonto.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblMonto.Location = new System.Drawing.Point(733, 527);
+            this.lblMonto.Name = "lblMonto";
+            this.lblMonto.Size = new System.Drawing.Size(60, 20);
+            this.lblMonto.TabIndex = 9;
+            this.lblMonto.Text = "label10";
+            
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(39, 545);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(129, 15);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "ESTADISTICA DE VENTA";
+            // 
+            // lbEstadisticas
+            // 
+            this.lbEstadisticas.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7});
+            this.lbEstadisticas.GridLines = true;
+            this.lbEstadisticas.Location = new System.Drawing.Point(35, 563);
+            this.lbEstadisticas.Name = "lbEstadisticas";
+            this.lbEstadisticas.Size = new System.Drawing.Size(607, 117);
+            this.lbEstadisticas.TabIndex = 11;
+            this.lbEstadisticas.UseCompatibleStateImageBehavior = false;
+            this.lbEstadisticas.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Tipo de Licencia";
+            this.columnHeader5.Width = 200;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Total unidades";
+            this.columnHeader6.Width = 200;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Monto total";
+            this.columnHeader7.Width = 200;
+            // 
+            // frmFacturas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(837, 692);
+            this.Controls.Add(this.lbEstadisticas);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.lblMonto);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lvRegistros);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtVendedor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmFacturas";
+            this.Text = "CONTROL DE REGISTRO DE FACTURA";
+            this.Load += new System.EventHandler(this.frmFacturas_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtVendedor;
+        private Label label3;
+        private Label lblFecha;
+        private GroupBox groupBox1;
+        private Button btnNuevaFact;
+        private Button btnRegistrarItem;
+        private TextBox txtUnidades;
+        private Label label7;
+        private ComboBox cboTipo;
+        private Label label6;
+        private TextBox txtNumFact;
+        private Label label5;
+        private Label label8;
+        private ListView lvRegistros;
+        private Label label9;
+        private Label lblMonto;
+        private Label label11;
+        private ListView lbEstadisticas;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+    }
+}
---
+++ pjControlFacturas/frmFacturas.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pjControlFacturas
+{
+    public partial class frmFacturas : Form
+    {
+        public frmFacturas()
+        {
+            InitializeComponent();
+        }
+
+        private void frmFacturas_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Now.ToShortDateString();
+            lblMonto.Text = (0).ToString("C");                  //Asi se cambia el monto y la fecha cuando se ingrsa por primera vez
+
+        }
+
+        private void btnNuevaFact_Click(object sender, EventArgs e)
+        {
+            txtNumFact.Clear();  //Limpiara la caja de texto de factura
+            txtUnidades.Clear();
+            cboTipo.Text = "(Seleccione el tipo)";
+            txtNumFact.Focus();
+        }
+
+        private void btnRegistrarItem_Click(object sender, EventArgs e)
+        {
+
+            int i;
+
+            //Capturar datos
+            int numFact = int.Parse(txtNumFact.Text);
+            string licencia = cboTipo.Text;
+            int unidades = int.Parse(txtUnidades.Text);
+
+            //Determinar el precio de las licencias
+            double precio = 0;
+
+            switch (licencia)
+            {
+                case "Cobre": precio = 700; break;
+                case "Bronce": precio = 900; break;
+                case "Silver": precio = 1400; break;
+                case "Gold": precio = 2500; break;
+            }
+
+            //Calcular el subtotal
+            double subtotal = unidades * precio;
+
+            //Enviando informacion a la lista de registro
+            ListViewItem fila = new ListViewItem(numFact.ToString());
+            fila.SubItems.Add(licencia);
+            fila.SubItems.Add(unidades.ToString());
+            fila.SubItems.Add(subtotal.ToString("0.00"));
+            lvRegistros.Items.Add(fila);
+
+            //Calculando el monto total acumulado
+            double mAacumulado = 0;
+            i = 0;
+
+            while(i < lvRegistros.Items.Count)
+            {
+               mAacumulado += double.Parse(lvRegistros.Items[i].SubItems[3].Text);
+                i++;
+            }
+            lblMonto.Text = mAacumulado.ToString("C");
+
+            //Calculando las estadisticas
+            int cCobre = 0, cBronce = 0, cSilver = 0, cGold = 0;            //categorias
+            double tCobre = 0, tBronce = 0, tSilver = 0, tGold = 0;         //monto
+
+            i = 0;
+
+            do 
+            { 
+                if(lvRegistros.Items[i].SubItems[1].Text == "Cobre")
+                {
+                    cCobre += int.Parse(lvRegistros.Items[i].SubItems[2].Text);
+                    tCobre += double.Parse(lvRegistros.Items[i].SubItems[3].Text);
+                }
+
+                else if(lvRegistros.Items[i].SubItems[1].Text == "Bronce")
+                {
+                    cBronce += int.Parse(lvRegistros.Items[i].SubItems[2].Text);
+                    tBronce += double.Parse(lvRegistros.Items[i].SubItems[3].Text);
+                }
+                else if(lvRegistros.Items[i].SubItems[1].Text == "Silver")
+                {
+                    cSilver += int.Parse(lvRegistros.Items[i].SubItems[2].Text);
+                    tSilver += double.Parse(lvRegistros.Items[i].SubItems[3].Text);
+                }
+                else if(lvRegistros.Items[i].SubItems[1].Text == "Gold")
+                {
+                    cGold += int.Parse(lvRegistros.Items[i].SubItems[2].Text);
+                    tGold += double.Parse(lvRegistros.Items[i].SubItems[3].Text);
+                }
+               i++;
+
+            }while(i < lvRegistros.Items.Count);
+
+            //Imprimiendo estadisticas
+
+            lbEstadisticas.Items.Clear();
+            string[] elementosFila = new string[3];
+            ListViewItem row;
+
+            elementosFila[0] = "Total Cobre";
+            elementosFila[1] = cCobre.ToString();
+            elementosFila[2] = tCobre.ToString();   
+            row = new ListViewItem(elementosFila);
+            lbEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Total Bronce";
+            elementosFila[1] = cBronce.ToString();
+            elementosFila[2] = tBronce.ToString();
+            row = new ListViewItem(elementosFila);
+            lbEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Total Silver";
+            elementosFila[1] = cSilver.ToString();
+            elementosFila[2] = tSilver.ToString();
+            row = new ListViewItem(elementosFila);
+            lbEstadisticas.Items.Add(row);
+
+            elementosFila[0] = "Total Gold";
+            elementosFila[1] = cGold.ToString();
+            elementosFila[2] = tGold.ToString();
+            row = new ListViewItem(elementosFila);
+            lbEstadisticas.Items.Add(row);
+        }
+
+    }
+}
